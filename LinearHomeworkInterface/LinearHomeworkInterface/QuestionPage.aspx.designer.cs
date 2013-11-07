cbf3554856aedcadff6f9f01953b27ff233b0845
Cooper640 <tjenkins01@drury.edu>
2013-11-07 15:31:20 -0600

    Set up pagination to reflect current question and represent other question in assignment (reflect progress through assignment). Set up spot to check that user hasn't tampered with current question parameter in url.

--- LinearHomeworkInterface/LinearHomeworkInterface/QuestionPage.aspx.designer.cs
+++ LinearHomeworkInterface/LinearHomeworkInterface/QuestionPage.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace LinearHomeworkInterface {
     public partial class QuestionPage {
         
         /// <summary>
+        /// paginationLiteral control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal paginationLiteral;
+        
+        /// <summary>
         /// question control.
         /// </summary>
         /// <remarks>
